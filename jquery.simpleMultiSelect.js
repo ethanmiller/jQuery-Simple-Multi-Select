abb2b867416e5e69db325c8992fa5efe94649057
Ethan Miller <millere@esem.(none)>
2010-03-23 00:04:17 -0700

    fixing up indentation

--- jquery.simpleMultiSelect.js
+++ jquery.simpleMultiSelect.js
@@ -13,39 +13,39 @@
 (function($){
     $.fn.simpleMultiSelect = function(options){
         var settings = $.extend({
-		highlight : '#CCE',
-		border : '#777',
-		width : 350,
-		height : 150,
-		classesOnly : false,
-		container : 'sms-container',
-		pseudoSelect : 'sms-pseudo-select',
-		selected : 'sms-selected',
-		unselected : 'sms-unselected'
-	    }, options);
+            highlight : '#CCE',
+            border : '#777',
+            width : 350,
+            height : 150,
+            classesOnly : false,
+            container : 'sms-container',
+            pseudoSelect : 'sms-pseudo-select',
+            selected : 'sms-selected',
+            unselected : 'sms-unselected'
+        }, options);
         return this.each(function(){
             // wrapping select in a div so that the select and 
-	    // pseudo select will be siblings
+            // pseudo select will be siblings
             $(this).wrap('<div class="' + settings.container + '"></div>');
             var divselect = $('<div class="' + settings.pseudoSelect + '"></div>');
-            $('option', this).each(function(){
+            $('option', this).each(function(){ 
                 var op = $(this);
                 var dv = $('<div/>')
-			 .text(op.text())
-			 .data('selected', op.attr('selected'));
-		// highlight pseudo option on load
-		toggleSelected(dv, settings);
+                    .text(op.text())
+                    .data('selected', op.attr('selected'));
+                // highlight pseudo option on load
+                toggleSelected(dv, settings);
                 dv.click(function(){
                     // we still have references to op and dv here ...
                     if(op.attr('selected')){
                         //de-select
                         op.removeAttr('selected');
-			dv.data('selected', false);
+                        dv.data('selected', false);
                         toggleSelected(dv, settings);
                     }else{
                         //select
                         op.attr('selected', true);
-			dv.data('selected', true);
+                        dv.data('selected', true);
                         toggleSelected(dv, settings);
                     }
                 });
@@ -58,7 +58,7 @@
                     cursor : "default",
                     overflow : "auto",
                     border : "1px solid " + settings.border
-		});
+                });
             }
             $(this).after(divselect).hide();
         });
@@ -67,7 +67,7 @@
         return this.each(function(){
             siblingDivSet(this).each(function(){
                 var psop = $(this);
-		if(psop.data('selected')){
+                if(psop.data('selected')){
                     psop.click();
                 }
             });
@@ -77,14 +77,14 @@
         return this.each(function(){
             siblingDivSet(this).each(function(){
                 var psop = $(this);
-		if(!psop.data('selected')){
+                if(!psop.data('selected')){
                     psop.click();
                 }
             });
         });
-    }
+    };
     function toggleSelected(elem, config){
-	var sel = elem.data('selected');
+        var sel = elem.data('selected');
         if(config.classesOnly){
             elem.toggleClass(config.selected, sel);
             elem.toggleClass(config.unselected, !sel);
