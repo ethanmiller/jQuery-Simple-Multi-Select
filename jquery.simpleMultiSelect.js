1cb22d2748d268ee3466ab8a8606e8250c837bde
Ethan Miller <millere@esem.(none)>
2010-03-23 02:48:38 -0700

    Added support for optgroup, and disabled options

--- jquery.simpleMultiSelect.js
+++ jquery.simpleMultiSelect.js
@@ -1,4 +1,4 @@
-/* jQuery Simple Multi-Select plugin 0.9
+/* jQuery Simple Multi-Select plugin 1.1
  *
  * Copyright (c) 2009 Ethan Miller
  *
@@ -21,36 +21,17 @@
             container : 'sms-container',
             pseudoSelect : 'sms-pseudo-select',
             selected : 'sms-selected',
-            unselected : 'sms-unselected'
+            unselected : 'sms-unselected',
+            disabled : 'sms-disabled',
+            optgroup : 'sms-optgroup',
+            optgroupLabel : 'sms-optgroup-label'
         }, options);
         return this.each(function(){
             // wrapping select in a div so that the select and 
             // pseudo select will be siblings
             $(this).wrap('<div class="' + settings.container + '"></div>');
             var divselect = $('<div class="' + settings.pseudoSelect + '"></div>');
-            $('option', this).each(function(){ 
-                var op = $(this);
-                var dv = $('<div/>')
-                    .text(op.text())
-                    .data('selected', op.attr('selected'));
-                // highlight pseudo option on load
-                toggleSelected(dv, settings);
-                dv.click(function(){
-                    // we still have references to op and dv here ...
-                    if(op.attr('selected')){
-                        //de-select
-                        op.removeAttr('selected');
-                        dv.data('selected', false);
-                        toggleSelected(dv, settings);
-                    }else{
-                        //select
-                        op.attr('selected', true);
-                        dv.data('selected', true);
-                        toggleSelected(dv, settings);
-                    }
-                });
-                divselect.append(dv);
-            });
+            buildFauxOptions($('> option, > optgroup', this), settings, divselect);
             if(!settings.classesOnly){
                 divselect.css({
                     width : settings.width,
@@ -85,19 +66,70 @@
     };
     function toggleSelected(elem, config){
         var sel = elem.data('selected');
+        var dis = elem.data('disabled')
         if(config.classesOnly){
             elem.toggleClass(config.selected, sel);
             elem.toggleClass(config.unselected, !sel);
+            elem.toggleClass(config.disabled, dis); // only happens onload
         }else{
             if(sel){
                 elem.css({'background-color' : config.highlight});
             }else{
                 elem.css({'background-color' : 'transparent'});
             }
+            if(dis){
+                elem.css({'color' : '#888'}); // only happens onload
+            }
         }
+    }
+    function buildFauxOptions(elements, settings, divselect){ 
+        elements.each(function(){
+            if(this.tagName == 'OPTGROUP'){
+                var subsel = $('<div/>');
+                var label = $('<div/>').text($(this).attr('label'));
+                subsel.append(label);
+                if(settings.classesOnly){
+                    subsel.addClass(settings.optgroup);
+                    label.addClass(settings.optgroupLabel);
+                }else{
+                    subsel.css({'padding-left' : '10px'});
+                    label.css({'font-weight' : 'bold'});
+                }
+                // recursive call here, using the same selector which meanse
+                // nested optgroup's are supported - however, it doesn't render them 
+                // nested. I'm not sure why - but in any case it matches html 4
+                buildFauxOptions($('> option, > optgroup', this), settings, subsel);
+                divselect.append(subsel);
+                return true;
+            }
+            var op = $(this);
+            var disabled = op.attr('disabled');
+            var dv = $('<div/>')
+                .text(op.text())
+                .data('selected', op.attr('selected'))
+                .data('disabled', disabled);
+            // highlight pseudo option on load
+            toggleSelected(dv, settings);
+            dv.click(function(){
+                if(disabled) return;
+                // we still have references to op and dv here ...
+                if(op.attr('selected')){
+                    //de-select
+                    op.removeAttr('selected');
+                    dv.data('selected', false);
+                    toggleSelected(dv, settings);
+                }else{
+                    //select
+                    op.attr('selected', true);
+                    dv.data('selected', true);
+                    toggleSelected(dv, settings);
+                }
+            });
+            divselect.append(dv);
+        });
     }
     function siblingDivSet(sel){
         // expects a select object, return jquery set
-        return $(sel).siblings('div').children('div');
+        return $(sel).siblings('div').find('div');
     }
 })(jQuery);
